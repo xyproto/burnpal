80030bc1309629355102e73cd3979f3db79f4044
Alexander F. Rødseth <rodseth@gmail.com>
2019-10-14 15:08:24 +0200

    Add a ColorPalette function

--- gfx.go
+++ gfx.go
@@ -2,8 +2,15 @@ package burnpal
 
 import (
 	"github.com/peterhellberg/gfx"
+	"image/color"
 )
 
+// GfxPalette returns the palette as a gfx.Palette
 func GfxPalette() gfx.Palette {
 	return gfx.Palette(Pal)
+}
+
+// ColorPalette returns the palette as a color.Palette
+func ColorPalette() color.Palette {
+	return gfx.Palette(Pal).AsColorPalette()
 }
